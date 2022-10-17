7893f10501904949c61f84970fe91ece2c94a1a7
brunocordioli072 <brunocordioli072@gmail.com>
2022-10-17 16:49:28 -0300

    fix: infinite loop on cli -h

--- epic7_bot/cli.py
+++ epic7_bot/cli.py
@@ -27,34 +27,34 @@ from dotenv import load_dotenv
 import sys
 
 
-stopped_start_checking_connection_problem = False
-
-
 def main():
-    def task():
-        args = docopt(__doc__, version="1.0.0", options_first=True)
+    args = docopt(__doc__, version="1.0.0", options_first=True)
 
-        if args['<command>'] != None:
-            load_dotenv()
-            init_logger()
-            setup_device()
-            threading.Thread(
-                target=start_checking_connection_problem, daemon=True).start()
+    def task():
+        load_dotenv()
+        init_logger()
+        setup_device()
+        threading.Thread(
+            target=start_checking_connection_problem, daemon=True).start()
+
         if args['<command>'] == "shop":
             start_auto_buy_secret_shop()
         elif args['<command>'] == "arena:npc":
             start_arena_npc_auto_battle()
-        elif args['<command>'] in [None]:
-            sys.argv.append('-h')
-            exit(main())
         else:
-            exit("%r is not a valid command." %
-                 args['<command>'])
+            return
 
-    thread = threading.Thread(
-        target=task, daemon=True)
-    thread.start()
-    while True:
-        if config.is_checking_connection_problem is False:
-            exit()
-        sleep(1)
+    if args['<command>'] in [None]:
+        sys.argv.append('-h')
+        exit(main())
+    elif args['<command>'] not in ["shop", "arena:npc"]:
+        exit("%r is not a valid command." %
+             args['<command>'])
+    else:
+        thread = threading.Thread(
+            target=task, daemon=True)
+        thread.start()
+        while True:
+            if config.is_checking_connection_problem is False:
+                exit()
+            sleep(1)
