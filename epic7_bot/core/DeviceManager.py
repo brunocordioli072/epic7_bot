f91478e4e7c4e5b228914bf198d2bc9b42eee67c
brunocordioli072 <brunocordioli072@gmail.com>
2022-10-25 15:07:29 -0300

    fix: Ensure ADB is running

--- epic7_bot/core/DeviceManager.py
+++ epic7_bot/core/DeviceManager.py
@@ -1,3 +1,5 @@
+import logging
+import subprocess
 from ppadb.client import Client
 from epic7_bot.utils.Singleton import Singleton
 
@@ -7,7 +9,12 @@ class DeviceManager(metaclass=Singleton):
         self.device = self.setup_device()
 
     def setup_device(self):
+        self.ensure_adb_is_running()
         client = Client(host="127.0.0.1", port=5037)
         device = client.devices()[0]
         device.shell("wm size 1600x900")
         return device
+
+    def ensure_adb_is_running(self):
+        logging.debug("Ensure ADB is running")
+        subprocess.run(["adb", "start-server"])
