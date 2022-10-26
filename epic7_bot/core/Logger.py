af4c3a9187da0155cc04d88805a3d90f8d89679a
brunocordioli072 <brunocordioli072@gmail.com>
2022-10-26 11:00:28 -0300

    fix: default log level should be INFO

--- epic7_bot/core/Logger.py
+++ epic7_bot/core/Logger.py
@@ -4,6 +4,6 @@ import os
 
 def init_logger():
     LOG_LEVEL = os.getenv("LOG_LEVEL") if os.getenv(
-        "LOG_LEVEL") is not None else "DEBUG"
+        "LOG_LEVEL") is not None else "INFO"
     logging.basicConfig(level=logging.getLevelName(LOG_LEVEL),
                         format='%(asctime)s :: %(levelname)s :: %(message)s')
