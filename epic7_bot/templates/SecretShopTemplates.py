75c19d54a5b4cab69d75df1ac968dd7b46d7db31
lucaseli712 <lucaselimalagoli@gmail.com>
2023-09-15 21:11:31 -0300

    testing fix

--- epic7_bot/templates/SecretShopTemplates.py
+++ epic7_bot/templates/SecretShopTemplates.py
@@ -8,21 +8,21 @@ class SecretShopTemplates(BaseTemplates):
         super(self.__class__, self).__init__()
 
         self.buy_button_covenant: Template = {
-            "image": cv2.imread(self.dirPath + '/../images/secret_shop/Buy_button_Covenant.png', 0),
+            "image": cv2.imread(self.dirPath + '/../images/secret_shop/Buy_button.png', 0),
             "name": "buy_button_covenant"
         }
 
         self.buy_button_mystic: Template = {
-            "image": cv2.imread(self.dirPath + '/../images/secret_shop/Buy_button_Mystic.png', 0),
+            "image": cv2.imread(self.dirPath + '/../images/secret_shop/Buy_button.png', 0),
             "name": "buy_button_mystic"
         }
 
         self.covenant: Template = {
-            "image": cv2.imread(self.dirPath + '/../images/secret_shop/covenant.png', 0),
+            "image": cv2.imread(self.dirPath + '/../images/secret_shop/friendship.png', 0),
             "name": "covenant"
         }
 
         self.mystic: Template = {
-            "image": cv2.imread(self.dirPath + '/../images/secret_shop/mystic.png', 0),
+            "image": cv2.imread(self.dirPath + '/../images/secret_shop/boots.png', 0),
             "name": "mystic"
         }
