5b2288deb20815b42c3554bd3e2b1c2fe3708076
brunocordioli072 <brunocordioli072@gmail.com>
2024-03-18 22:12:19 -0300

    fix: scroll to bottom on logs

--- packages/app/src/components/LogsContainer/LogsContainer.tsx
+++ packages/app/src/components/LogsContainer/LogsContainer.tsx
@@ -2,21 +2,26 @@ import { useEffect, useRef } from "react";
 import { useAppContext } from "../../context/AppContext";
 import './LogsContainer.css'
 
+const AlwaysScrollToBottom = () => {
+    const elementRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        if (elementRef.current)
+            elementRef.current.scrollIntoView({ behavior: 'smooth' })
+    });
+    return <div ref={elementRef} />;
+};
+
+
 const Logs: React.FC = () => {
     const {
         logs,
     } = useAppContext()
-    const logsRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        if (logsRef && logsRef.current)
-            logsRef.current.scrollIntoView()
-    }, []);
 
     return (
-        <div ref={logsRef} className='logs'>
+        <div className='logs'>
             <div className="title">Logs</div>
             {...logs}
+            <AlwaysScrollToBottom />
         </div>
     )
 }
